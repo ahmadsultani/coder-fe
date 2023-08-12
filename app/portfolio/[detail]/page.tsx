5fc7bfd7c9b6e649a066f68e84c1c5b9298cf8eb
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2023-08-13 02:25:36 +0800

    initial commit

---
+++ app/portfolio/[detail]/page.tsx
@@ -0,0 +1,13 @@
+/***
+ * This is a dynamic route
+ * The page will be generated for each portfolio item
+ * bassed on the params passed in the url
+ *
+ * e.g. /portfolio/sappe-porto or /portfolio/sappe-movie-app
+ */
+
+export default function DetailPage() {
+  return (
+    <div>page</div>
+  )
+}
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import ExampleAtom from "@atoms/ExampleAtom";
+
+export default function PortfolioPage() {
+  return (
+    <div>
+      <ExampleAtom exampleProp="prop" exampleProp2={2} />
+    </div>
+  );
+}
