761702ef8b16cc3d6ccdccdc0bb38127ec728414
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2023-08-17 13:35:01 +0800

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,75 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        neutral: {
+          light: "#E4E4E4",
+          "light-hover": "#DEDEDE",
+          "light-active": "#D7D7D7",
+          normal: "#C2C2C2",
+          "normal-hover": "#A5A5A5",
+          "normal-active": "#8E8E8E",
+          dark: "#444444",
+          "dark-hover": "#313131",
+          "dark-active": "#151515",
+        },
+        primary: {
+          light: "#FDF8E6",
+          "light-hover": "#FCF4D9",
+          "light-active": "#F9E9B1",
+          normal: "#EBB702",
+          "normal-hover": "#D4A502",
+          "normal-active": "#BC9202",
+          dark: "#B08902",
+          "dark-hover": "#8D6E01",
+          "dark-active": "#6A5201",
+          darker: "#524001",
+        },
+        secondary: {
+          black: "#151515",
+          white: "#FFFFFF",
+        },
+      },
+      fontSize: {
+        "text-s": [
+          "12px",
+          {
+            lineHeight: "16px",
+          },
+        ],
+        "text-m": [
+          "14px",
+          {
+            lineHeight: "20px",
+          },
+        ],
+        "text-l": [
+          "16px",
+          {
+            lineHeight: "24px",
+          },
+        ],
+        "heading-s": [
+          "20px",
+          {
+            lineHeight: "28px",
+          },
+        ],
+        "heading-m": [
+          "28px",
+          {
+            lineHeight: "36px",
+          },
+        ],
+        "heading-l": [
+          "36px",
+          {
+            lineHeight: "44px",
+          },
+        ],
       },
     },
   },
