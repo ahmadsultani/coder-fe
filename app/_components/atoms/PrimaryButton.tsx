7454d8154c3f423c9b3b6607582a151cfaf7bb71
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-08-31 21:05:51 +0800

    fix:28 button atomic component

--- app/_components/atoms/PrimaryButton.tsx
+++ app/_components/atoms/PrimaryButton.tsx
@@ -2,37 +2,20 @@ import Link from "next/link";
 import Image from "next/image";
 
 interface PrimaryButtonProps {
-  btnText: string;
-  btnLinkTo?: string;
-  showIcon?: boolean;
+  children: React.ReactNode;
   iconLeft?: boolean;
-  iconSrc?: string;
+  icon?: React.ReactElement;
 }
 const PrimaryButton = (props: PrimaryButtonProps) => {
-  const {
-    btnLinkTo = "",
-    btnText,
-    showIcon = false,
-    iconLeft = true,
-    iconSrc = "/",
-  } = props;
+  const { children, iconLeft = false, icon } = props;
   const flexDirection = iconLeft ? "flex-row-reverse" : "flex-row";
-  const visibility = showIcon ? "" : "hidden";
   return (
-    <Link href={btnLinkTo}>
-      <button
-        className={`flex ${flexDirection}  gap-1 items-center p-[12px_24px] bg-primary-normal rounded-lg`}
-      >
-        <span className="text-white text-heading-s font-medium">{btnText}</span>
-        <Image
-          src={iconSrc}
-          alt=""
-          height={20}
-          width={20}
-          className={`${visibility}`}
-        />
-      </button>
-    </Link>
+    <button
+      className={`flex ${flexDirection}  items-center gap-1 rounded-lg bg-primary-normal p-[12px_24px] duration-300 hover:bg-primary-normal-hover `}
+    >
+      <span className="text-heading-s font-medium text-white">{children}</span>
+      {icon}
+    </button>
   );
 };
 
