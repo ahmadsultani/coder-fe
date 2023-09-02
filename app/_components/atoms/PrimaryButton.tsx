d81bdba96985be72868bf5c29006a60d8de474be
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-09-02 08:21:04 +0800

    feat: new primary button

--- app/_components/atoms/PrimaryButton.tsx
+++ app/_components/atoms/PrimaryButton.tsx
@@ -1,20 +1,12 @@
-import Link from "next/link";
-import Image from "next/image";
-
 interface PrimaryButtonProps {
   children: React.ReactNode;
-  iconLeft?: boolean;
-  icon?: React.ReactElement;
 }
+
 const PrimaryButton = (props: PrimaryButtonProps) => {
-  const { children, iconLeft = false, icon } = props;
-  const flexDirection = iconLeft ? "flex-row-reverse" : "flex-row";
+  const { children } = props;
   return (
-    <button
-      className={`flex ${flexDirection}  items-center gap-1 rounded-lg bg-primary-normal p-[12px_24px] duration-300 hover:bg-primary-normal-hover `}
-    >
-      <span className="text-heading-s font-medium text-white">{children}</span>
-      {icon}
+    <button className="flex items-center gap-1 rounded-[4px] bg-primary-normal p-[12px_24px] text-heading-s font-medium text-primary-light duration-300 hover:bg-primary-normal-hover">
+      {children}
     </button>
   );
 };
