0c67207d1795b201d9ed11fc7ea138598986fc94
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-09-02 09:05:09 +0800

    fix: logo text gap

--- app/_components/molecules/LogoText.tsx
+++ app/_components/molecules/LogoText.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function LogoText() {
   return (
-    <section className="flex items-center gap-4">
+    <section className="flex items-center gap-2">
       <Link href="/">
         <LogoIcon size={46} />
       </Link>
-      <div className="text-m flex flex-col font-bold leading-tight">
+      <div className="flex flex-col text-text-m font-bold leading-tight">
         <h1 className="">CODER INSTITUTE HIMPUNAN MAHASISWA INFORMATIKA</h1>
         <h1>FAKULTAS TEKNIK UNIVERSITAS HASANUDDIN</h1>
       </div>
