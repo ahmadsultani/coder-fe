88546983ab3652302035da55d2cc4ce342c2a4a5
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-09-13 16:05:32 +0800

    fix: layout

--- app/_components/atoms/PrimaryButton.tsx
+++ app/_components/atoms/PrimaryButton.tsx
@@ -5,7 +5,7 @@ interface PrimaryButtonProps {
 
 const PrimaryButton = (props: PrimaryButtonProps) => {
   const { fullWidth = false, children } = props;
-  const width = fullWidth ? "w-fit" : "w-full";
+  const width = fullWidth ? "w-full" : "w-fit";
   return (
     <button
       className={`${width} flex items-center gap-2 rounded bg-primary-normal p-[6px_12px] text-text-l font-medium text-primary-light duration-300 hover:bg-primary-normal-hover lg:p-[12px_24px] lg:text-heading-s`}
--- app/_components/organisms/ContactUs.tsx
+++ app/_components/organisms/ContactUs.tsx
@@ -8,7 +8,7 @@ function ContactUs() {
       <section className="relative flex w-full flex-col items-center justify-center gap-11 overflow-hidden rounded-2xl border-2 border-solid border-neutral-light-active px-7 py-[52px] text-center shadow-[1px_1px_4px_0px_rgba(0,0,0,0.25)] lg:px-16">
         <section className="relative">
           <Image
-            className="absolute -left-14 -top-6 z-20"
+            className="absolute -left-14 -top-6"
             src="/decorations/star-four.svg"
             width={69}
             height={69}
@@ -27,58 +27,54 @@ function ContactUs() {
             Jangan malu-malu, ayo kontak kami, dan buat hal menarik!
           </p>
         </section>
-        <section className="z-10 flex flex-col justify-center gap-5 md:flex-row lg:gap-[180px]">
-          <section>
-            <Link href={"https://www.linkedin.com/company/coder-institute"}>
-              <PrimaryButton>
-                <Image
-                  src="/icons/icon_linkedin_.svg"
-                  width={20}
-                  height={20}
-                  alt="icon-linkedin"
-                />
-                <p className="underline">Linkedin</p>
-              </PrimaryButton>
-            </Link>
-          </section>
-          <section>
-            <Link href={"https://www.instagram.com/coderinstitute/"}>
-              <PrimaryButton>
-                <Image
-                  src="/icons/icon_instagram_.svg"
-                  width={20}
-                  height={20}
-                  alt="icon-instagram"
-                />
-                <p className="underline">Instagram</p>
-              </PrimaryButton>
-            </Link>
-          </section>
+        <section className="z-10 flex flex-col gap-x-16 gap-y-5 md:flex-row">
+          <Link href={"https://www.linkedin.com/company/coder-institute"}>
+            <PrimaryButton fullWidth>
+              <Image
+                src="/icons/icon_linkedin_.svg"
+                width={20}
+                height={20}
+                alt="icon-linkedin"
+              />
+              <p className="underline">Linkedin</p>
+            </PrimaryButton>
+          </Link>
+          <Link href={"https://www.instagram.com/coderinstitute/"}>
+            <PrimaryButton fullWidth>
+              <Image
+                src="/icons/icon_instagram_.svg"
+                width={20}
+                height={20}
+                alt="icon-instagram"
+              />
+              <p className="underline">Instagram</p>
+            </PrimaryButton>
+          </Link>
         </section>
-        <section className="absolute inset-0">
+        <section className="absolute inset-0 -z-0">
           <Image
-            className="absolute -bottom-10 -left-10 z-10 scale-[.65] transform lg:bottom-0 lg:left-0 lg:scale-100"
+            className="absolute -bottom-10 -left-10 scale-[.65] lg:bottom-0 lg:left-0 lg:scale-100"
             src="/decorations/circle.svg"
             width={194}
             height={194}
             alt="bg-circle"
           />
           <Image
-            className="absolute -left-8 bottom-12 z-20 scale-[.65] transform lg:-left-3 lg:bottom-24 lg:scale-100 "
+            className="absolute -left-8 bottom-12 scale-[.65] lg:-left-3 lg:bottom-24 lg:scale-100 "
             src="/decorations/star-six.svg"
             width={141}
             height={11}
             alt="bg-star-six"
           />
           <Image
-            className="scale- absolute -right-10 -top-10 z-10 scale-[.65] transform lg:right-0 lg:top-0 lg:scale-100"
+            className="absolute -right-10 -top-10 scale-[.65] lg:right-0 lg:top-0 lg:scale-100"
             src="/decorations/circle2.svg"
             width={194}
             height={194}
             alt="bg-circle"
           />
           <Image
-            className="absolute -right-8 top-12 z-20 scale-[.65] transform lg:-right-3 lg:top-24 lg:scale-100"
+            className="absolute -right-8 top-12 scale-[.65] lg:-right-3 lg:top-24 lg:scale-100"
             src="/decorations/star2-six.svg"
             width={141}
             height={141}
