d7128d7ecdf21df3090fd288695431704d420964
Nadiyah Amaliyah <118332537+naddiyh@users.noreply.github.com>
2023-09-13 17:34:27 +0800

    feat : 30 responsive contact us
    * feat : 30 responsive contact us
    
    * fix: layout
    
    ---------
    
    Co-authored-by: ahmadsultani <dayanullahas21d@student.unhas.ac.id>

--- app/_components/atoms/PrimaryButton.tsx
+++ app/_components/atoms/PrimaryButton.tsx
@@ -1,11 +1,15 @@
 interface PrimaryButtonProps {
   children: React.ReactNode;
+  fullWidth?: boolean;
 }
 
 const PrimaryButton = (props: PrimaryButtonProps) => {
-  const { children } = props;
+  const { fullWidth = false, children } = props;
+  const width = fullWidth ? "w-full" : "w-fit";
   return (
-    <button className="flex w-fit items-center gap-2 rounded bg-primary-normal p-[6px_12px]  text-text-m font-medium text-primary-light duration-300 hover:bg-primary-normal-hover lg:p-[12px_24px] lg:text-heading-s">
+    <button
+      className={`${width} flex items-center gap-2 rounded bg-primary-normal p-[6px_12px] text-text-l font-medium text-primary-light duration-300 hover:bg-primary-normal-hover lg:p-[12px_24px] lg:text-heading-s`}
+    >
       {children}
     </button>
   );
