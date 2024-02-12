8da25e15dbd821123d3c262aaf6136171a602f4b
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2024-02-12 09:52:17 +0800

    update hero image

--- app/_components/organisms/Hero.tsx
+++ app/_components/organisms/Hero.tsx
@@ -4,10 +4,10 @@ import PrimaryButton from "../atoms/PrimaryButton";
 const HeroSection = () => {
   return (
     <section
-      className="flex items-center gap-24 px-7 py-8 lg:pl-16 lg:pr-0 "
+      className="flex min-h-[480px] items-center gap-32 px-7 py-8 lg:pl-16 lg:pr-0 "
       id="beranda"
     >
-      <section className="flex flex-1 flex-col gap-10">
+      <section className="flex  flex-1 flex-col gap-10">
         <header className="flex flex-col gap-2 font-bold">
           <h1 className="text-heading-m text-primary-normal sm:text-heading-l lg:whitespace-nowrap lg:text-heading-xl">
             CODER INSTITUTE <br />
@@ -39,15 +39,16 @@ const HeroSection = () => {
       </section>
       <section className="relative -z-50 hidden lg:block">
         <Image
-          src="/images/coder-fotbar.jpeg"
+          src="/images/hero-image.webp"
           width={631}
           height={733}
           style={{
-            objectFit: "cover",
-            width: "631px",
-            height: "733px",
-            objectPosition: "50% 75%",
+            objectFit: "contain",
+            width: "auto",
+            height: "auto",
           }}
+          priority
+          quality={100}
           alt="Hero Image"
         />
         <Image
